60cde071c902e517a9f95fefae61863241529c19
Poojani Wijenayake <poojaniwijenayake@gmail.com>
2025-11-17 21:44:47 +0530

    update

--- taskManager.js
+++ taskManager.js
@@ -1,1 +1,7 @@
 console.log( "Welcome to Task Manager!");
+console.log( "1. List tasks");
+console.log( "2. Add new task");
+console.log( "3. Mark task as complete");
+console.log( "4. Delete task");
+console.log( "5. Exit");
+
