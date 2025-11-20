b3aa8aa7de98b8c1210a1a33fb35c3ec30866af2
Poojani Wijenayake <poojaniwijenayake@gmail.com>
2025-11-20 20:28:36 +0530

    updates inputs

--- taskManager.js
+++ taskManager.js
@@ -1,3 +1,10 @@
+const readline = require("readline");
+const r = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function menu() {
 console.log( "Welcome to Task Manager!");
 console.log( "1. List tasks");
 console.log( "2. Add new task");
@@ -5,4 +12,30 @@ console.log( "3. Mark task as complete");
 console.log( "4. Delete task");
 console.log( "5. Exit");
 
+ r.question("Select an option: ", handleMenu);
+}
+
+function handleMenu(choice) {
+  switch (choice) {
+    case "1": 
+       listTasks(); 
+       break;
+    case "2": 
+       addTask(); 
+       break;
+    case "3": 
+       completeTask(); 
+       break;
+    case "4": 
+       deleteTask(); 
+       break;
+    case "5": 
+        console.log("Exiting program. Goodbye!"); 
+        rl.close(); 
+        break;
+    default: 
+        console.log("Invalid choice!"); 
+        showMenu();
+  }
+}
 
